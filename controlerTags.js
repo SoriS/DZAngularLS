2c9a0124d1b9c21b467636b42081434bffd335bc
SoriS <pashanikoluyk@mail.ru>
2017-05-25 15:56:26 +0300

    color tags

--- controlerTags.js
+++ controlerTags.js
@@ -1,19 +1,39 @@
 submitExample.controller('BasicDemoCtrl', function ($scope, $timeout, $q) {
     var self = this;
-    
+
     self.readonly = false;
 
     // Lists of fruit names and Vegetable objects
     self.FruitNames = angular.copy(self.tags);
 
-    self.tags = ['zxczxczx' , 'zzxczxc' ]
+    self.tags = [
+        {
+            name: 'zxczxczx1',
+            style: "red"
+        },
+        {
+            name: 'zzxczxc2',
+            style: "blue"
+        }]
 
     self.newVeg = function ($chip) {
-        if(self.tags.indexOf($chip) != -1)
-        {return}
+        var tagsName = self.tags.map(e=>e.name)
+        if (self.tags.indexOf(tagsName) != -1)
+        {
+            return
+        }
         console.log($chip)
-       self.tags.push( $chip );
+        self.tags.push($chip);
     }
-    
-    
+
+
+}).directive('customChip', function () {
+    return {
+        restrict: 'A',
+        link: function (scope, elem, attrs) {
+            var chipClass = scope.$chip.style;
+            var mdChip = elem.parent().parent();
+            mdChip.addClass(chipClass);
+        }
+    }
 });
