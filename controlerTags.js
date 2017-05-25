c497a535781c500a9b9bd0677e2ec4b1824b45dd
SoriS <pashanikoluyk@mail.ru>
2017-05-25 12:02:28 +0300

    fix delete

--- controlerTags.js
+++ controlerTags.js
@@ -4,19 +4,16 @@ submitExample.controller('BasicDemoCtrl', function ($scope, $timeout, $q) {
     self.readonly = false;
 
     // Lists of fruit names and Vegetable objects
-    self.FruitNames = angular.copy(self.tags2);
+    self.FruitNames = angular.copy(self.tags);
 
-    self.tags2 = ['zxczxczx' , 'zzxczxc' ]
+    self.tags = ['zxczxczx' , 'zzxczxc' ]
 
     self.newVeg = function ($chip) {
-        if(self.tags2.indexOf($chip) != -1)
+        if(self.tags.indexOf($chip) != -1)
         {return}
         console.log($chip)
-       self.tags2.push( $chip );
+       self.tags.push( $chip );
     }
-    self.remove = function ($chip) {
-        var ind = self.tags2.indexOf({$chip})
-        self.tags2.splice(ind,1);
-    }
+    
     
 });
